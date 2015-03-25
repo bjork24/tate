ea37dbcdc3845c7dbe4073f2f74f9d61f0a8d4fe
Dan Chilton <dan.chilton@gmail.com>
2015-03-25 10:26:35 -0500

    WIP cli working as expected

--- bin/cli.js
+++ bin/cli.js
@@ -2,19 +2,56 @@
 
 'use strict';
 
+var _ = require('lodash');
 var cli = require('commander');
-var chalk = require('chalk');
-
-function list(val) {
-  return val.split(',');
-}
+var tate = require('../lib/tate');
+var log = require('../lib/utils').log;
 
 cli
   .version(require('../package.json').version)
-  .option('-d, --directories [path]', 'Paths to ignore, comma seperated', list)
+  .usage('[options] <file ...>')
+  .option('-s, --sass', 'Annotate .sass and .scss files')
+  .option('-l, --less', 'Annotate .less files')
+  .option('-y, --styl', 'Annotate .styl files')
+  .option('-c, --css', 'Annotate .css files')
+  .option('-a, --append', 'Output is appended to each matching file')
+  .option('-m, --manifest', 'Output is saved to a manifest file')
+  .option('-t, --terminal', 'Output is displayed in terminal only')
+  .option('-i, --ignore [path]', 'Paths to ignore, comma seperated NO SPACES')
   .parse(process.argv);
 
-if (!cli.directories) {
-  console.log(chalk.red('\n No directory specified.'));
+var opts = {};
+
+if ( cli.args.length ) {
+  var files = cli.args;
+} else {
+  log.warn('No input file or directory specified.');
   cli.help();
 }
+
+if ( cli.ignore ) {
+  _.extend(opts, { ignore: cli.ignore.split(',') });
+}
+
+if ( cli.sass || cli.less || cli.styl || cli.css ) {
+  var ext = [];
+  if (cli.sass) { ext.push('.sass', '.scss'); }
+  if (cli.less) { ext.push('.less'); }
+  if (cli.styl) { ext.push('.styl'); }
+  if (cli.css)  { ext.push('.css'); }
+  _.extend(opts, { ext: ext });
+} else {
+  log.warn('No file type specified. Defaulting to .css');
+}
+
+if ( cli.append || cli.manifest || cli.terminal ) {
+  var output = [];
+  if (cli.append) { output.push('append'); }
+  if (cli.manifest) { output.push('manifest'); }
+  if (cli.terminal)  { output.push('terminal'); }
+  _.extend(opts, { output: output });
+} else {
+  log.warn('No output type specified. Defaulting to terminal');
+}
+
+tate(files, opts);
